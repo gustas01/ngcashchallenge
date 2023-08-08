33188f21dcafa0d00f96e5a23679052c76b4002a
Gustavo <gustavohdlima@hotmail.com>
2023-08-08 14:19:48 -0300

    Feat: Adicionando Spring Security
    Configurações básicas iniciais do Spring Security

--- src/main/java/com/gustavo/ngcashchallenge/models/User.java
+++ src/main/java/com/gustavo/ngcashchallenge/models/User.java
@@ -5,13 +5,18 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
@@ -36,4 +41,32 @@ public class User {
   @NotNull
   @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
   private Account account = new Account();
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return switch (this.role){
+      case USER -> List.of(new SimpleGrantedAuthority("USER"));
+      case ADMIN -> List.of(new SimpleGrantedAuthority("ADMIN"), new SimpleGrantedAuthority("USER"));
+    };
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
